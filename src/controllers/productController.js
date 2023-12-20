c787ede3157969e6c30bb59b3d432e3f230cf7c4
SoleSmith <fernanda.smith.ss@gmail.com>
2023-12-19 22:56:48 -0300

    edit editarProdController y updateProdController

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,9 +1,20 @@
+const fs = require('fs');
+
+const path = require('path');
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+function getProducts() {return JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));}
+
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 const prodController = {
     indexProductController: (req, res) => {
       res.render ('../views/mascotas.ejs', {
         title: 'Mascotas'
       });
     },
+
     detailProductController:(req, res) => {
         idProducto = parseInt(req.params.id);
         if (idProducto === 1 ){
@@ -19,33 +30,58 @@ const prodController = {
           title: 'Crear producto',
       });
     },
-    //ruta para editar prod
-    editarProdController:(req, res) => {
-      idProducto = parseInt(req.params.idProd);
-      producto = {
-        mascota: 'perro',
-        nombre: 'Pipeta',
-        descuento : 15,
-        descripcion : 'PARA EL TRATAMIENTO, CONTROL Y PREVENCIÓN CONTRA PULGAS Y GARRAPATAS PARA PERROS Y GATOS.SE RECOMIENDA LA APLICACIÓN MENSUAL DEL PRODUCTO. SE PUEDE APLICAR EN CACHORROS DESDE LAS 8 SEMANAS DE VIDA Y DE 1 KG DE PESO.',
-        precio: 5200,
-        categoria: 'cuidados-higiene',
-        edadMascota : 'adulto',
-        tamañoMascota:'grande',
-        marca :'absorsol',
-        destacado : 'si'
-      }
 
-      res.render('../views/products/editar.ejs', {
-          title: 'Editar producto',
-          producto
-      });
+    //Controlador para editar prod -sole
+    editarProdController:(req, res) => {
+      const productos =getProducts();
+      const {id} = req.params;		
+      const producto = productos.find((product)=> product.id ==id);
+  
+      res.render("../views/products/editar.ejs", {producto});
     },
 
+    //Guardar del crear Prod.
     guardarProd:(req, res) => {
       res.render ('../views/mascotas.ejs', {
         title: 'Mascotas'
       });
     },
+
+    //Guardar del Editar -Sole
+    updateProdController: (req, res) => {		
+      const productos =getProducts();
+      const {id} = req.params;
+
+      let subCatFinal = "";
+      
+      if (req.body.subCat != "") {
+        subCatFinal = req.body.subCat;
+      } else if (req.body.subCatAcc != ""){
+        subCatFinal = req.body.subCatAcc;
+      };
+
+      const esDestacado = (req.body.destacado === "true") ? "si" : "no";
+                  
+      const productToEdit = productos.findIndex((product)=> product.id ==id);
+      productos[productToEdit].nombreProducto= req.body.nombreprod;
+      productos[productToEdit].descripcion = req.body.descripcion;
+      productos[productToEdit].precio = parseInt(req.body.precio);
+      productos[productToEdit].descuento = parseInt(req.body.descuento);
+      productos[productToEdit].mascota = req.body.mascota;
+      productos[productToEdit].imagen = req.file ? req.file.filename : productos[productToEdit].imagen;
+      productos[productToEdit].marca = req.body.marca;
+      productos[productToEdit].edadMascota = req.body.edadmascota;
+      productos[productToEdit].tamanioMascota = req.body.tamaniomascota;
+      productos[productToEdit].destacado = esDestacado;
+      productos[productToEdit].categoria = req.body.categoria;
+      productos[productToEdit].subCategoria = subCatFinal;
+      productos[productToEdit].presentacion = req.body.presentacion;
+    
+      fs.writeFileSync(productsFilePath, JSON.stringify(productos));
+      
+      res.redirect("/product");          
+    },
+
 
     //ruta /product/perros o /gatos
     productsListController:(req, res) => {
