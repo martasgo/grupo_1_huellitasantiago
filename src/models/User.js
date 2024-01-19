c934fea135076b54be2e8deba1433d63cdeddd36
Marta <mabutti@gmail.com>
2024-01-19 17:32:31 -0300

    add model for User methods

---
+++ src/models/User.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const path = require("path");
+
+// Objeto User que contiene los métodos para manipular el JSON de usuarios
+const User = {
+    // Ruta hacia el JSON de usuarios
+    fileName: path.join(__dirname , "../data/users.json"),
+    // Conversión del JSON de usuarios a array para poder trabajarlo con JS 
+    getData: function () {
+        return JSON.parse(fs.readFileSync(this.fileName , "utf-8"));
+    },
+    // Método que obtiene a todos los usuarios
+    findAll: function () {
+        return this.getData();
+    },
+    // Método para encontrar un usuario por ID
+    getByPk: function(id) {
+        let allUsers = this.findAll();
+        let userFound = allUsers.find(oneUser => oneUser.id === id);
+	    return userFound;
+    },
+    // Método para encontrar un usuario por campo de formulario
+    getByField: function(field , text) {
+        let allUsers = this.findAll();
+        let userFound = allUsers.find(oneUser => oneUser[field] === text);
+        return userFound;
+    },
+    
+    // Método para eliminar un usuario por su ID
+    delete: function (id) {
+        let allUsers = this.findAll();
+        let finalUsers = allUsers.filter (oneUser => oneUser.id !== id);
+        fs.writeFileSync(this.fileName , JSON.stringify(finalUsers , null , " ") );
+	    return true;
+    },
+    updateList: function(user){
+        fs.writeFileSync(this.fileName, JSON.stringify(user), {
+            flag:"w",
+            encoding:"utf-8",
+        });
+	    return true;
+    }
+};
+
+module.exports = User;
