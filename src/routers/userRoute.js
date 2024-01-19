d668a8a2680e87c33ba4a8df26541e9807f20312
Marta <mabutti@gmail.com>
2024-01-19 17:32:54 -0300

    add routes for get/post registration form

--- src/routers/userRoute.js
+++ src/routers/userRoute.js
@@ -1,16 +1,40 @@
 const { Router } = require("express");
 const userController = require("../controllers/userController");
+const registerValidations = require("../middlewares/registerValidations");
 
+const path = require("path");
+const multer = require("multer");
 const routerUsers = Router();
+
+// Multer - manejo del almacenamiento
+const storage = multer.diskStorage({
+	destination: (req , file , cb) => {
+		cb (null , path.resolve(__dirname , "../../public/images/usuarios"));
+	},
+	filename: (req , file , cb) => {
+		cb (null , file.fieldname + "-" + Date.now() + path.extname(file.originalname))
+	}
+});
+
+// Instanciar multer para manejar los métodos
+const upload = multer ({ storage });
 
 const routesUser = {
     loginRoute: "/login",
     registerRoute: "/register",
-    profileRoute: "/profile"
+    profileRoute: "/profile",
+	listUsersRoute: "/usersList"
 };
 
 routerUsers.get(routesUser.loginRoute, userController.loginController);
+
+// get-post form registración
 routerUsers.get(routesUser.registerRoute, userController.registerController);
+routerUsers.post(routesUser.registerRoute, registerValidations, upload.single("foto"), userController.addRegisterController);
+
 routerUsers.get(routesUser.profileRoute, userController.profileController);
+
+routerUsers.get(routesUser.listUsersRoute, userController.listUsersController);
+
 
 module.exports = routerUsers;
