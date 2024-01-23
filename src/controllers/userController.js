6448ee53ed8e0c44899cf78e03960dd495da6944
SoleSmith <fernanda.smith.ss@gmail.com>
2024-01-23 11:55:15 -0300

    update methods for editUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,10 @@
 const { validationResult } = require ("express-validator");
 const User = require ("../models/User");
-let bcrypt = require ("bcrypt");
+let bcrypt = require ("bcryptjs");
 
 const fs = require('fs');
 const path = require('path');
 const usersFilePath = path.join(__dirname, '../data/users.json');
-
 
 const userController = {
     loginController: (req, res) => {
@@ -81,74 +80,81 @@ const userController = {
 // Controlladores de edición de registro de usuario GET y PUT
     editController: (req, res)=>{
       //res.send('llega la info');
+     // const user = req.session.userLogged || {};
       const idUser = parseInt(req.params.idUser);
-      const infoUser = User.findByPk(idUser);
+      const infoUser = User.getByPk(idUser);
       console.log(infoUser);
-      res.render("../views/users/editUser.ejs",{title: 'Edición de usuarios', infoUser})
-
+      res.render("../views/users/editUser.ejs",{
+        title: 'Edición de usuarios',
+       // user,
+        infoUser
+      })
     },
     updateEditController: (req, res)=>{
       const resultValidation = validationResult(req);
-
+      //console.log(resultValidation);
+      //console.log(req.body);
+      //console.log(req.file);
+      let idUser = parseInt(req.params.idUser);
+      let infoUser = User.getByPk(idUser);
+      let imagen = req.body.imageAnt;
       if ( resultValidation.errors.length > 0){
+        // si hay nueva imagen la borro hasta tener todo ok
+        if (req.file) {
+          User.deleteImagen(req.file.filename);
+        }
         res.render('../views/users/editUser.ejs', {
           title: 'Edición de usuarios',
           errors: resultValidation.mapped(),
-          oldData: req.body
+          oldData: req.body,
+          infoUser,
+          imagen
         });
-      }
-      
-      let userInDB = User.getByField("email" , req.body.email );
-
-      if (userInDB) {
-        // No se hace la edición del registro porque ya existe. 
-          res.render('../views/users/editUser.ejs',{
-          title: 'Edición de usuarios',
-          errors: {
-            email: {
-              msg: 'Este email ya está registrado'
+      } else {
+          // se verifica el mail no esté registrado
+          let userInDB = User.getByField("email" , req.body.email);
+          //console.log(userInDB);
+          //console.log(infoUser);
+          if (userInDB.id !== infoUser.id) {
+            //si existe el mail significa que no se puede guardar al edicion y borro la imagen que se ha subido
+            if (req.file) {
+              User.deleteImagen(req.file.filename);
             }
-          },
-          oldData: req.body
-        });
-      };
-      //Cuando el usuario exite, se edita la información y se guarda.
-      const {idUser} = req.params;
-      const infoUser = User.findByPk(idUser);
+            // No se hace la edición del registro porque ya existe ese mail. 
+              res.render('../views/users/editUser.ejs',{
+              title: 'Edición de usuarios',
+              errors: {
+                email: {
+                  msg: 'Este email ya está registrado'
+                }
+              },
+              oldData: req.body,
+              infoUser,
+              imagen
+            });
+          } else {
+            //Cuando el usuario exite, se edita la información y se guarda.
+            let idUser = parseInt(req.params.idUser);
+            const infoUser = User.getByPk(idUser);
 
-      let updateUser = User.findByPk((user)=>user.id === idUser);
-      infoUser[updateUser].nombreUsuario = req.body.nombre;
-      infoUser[updateUser].apellidoUsuario = req.body.apellido;
-      infoUser[updateUser].emailUsuario = req.body.email;
-      infoUser[updateUser].direccionUsuario = req.body.direccion;
-      infoUser[updateUser].telefonoUsuario = req.body.telefono;
-      infoUser[updateUser].imagenUsuario = req.body.imagen;
+            //obtengo todo el listado de usuarios
+            let allUser = User.getData();
+            //busco el indice al que corresponde el usuario que estoy editando
+            const updateUser = allUser.findIndex((user)=> user.id ==idUser);
+            allUser[updateUser].nombre = req.body.nombre;
+            allUser[updateUser].apellido = req.body.apellido;
+            allUser[updateUser].email = req.body.email;
+            allUser[updateUser].direccion = req.body.dir;
+            allUser[updateUser].telefono = req.body.telefono;
+            allUser[updateUser].categoria = req.body.categoria;
+            allUser[updateUser].contraseña = bcrypt.hashSync(req.body.contrasenia , 10 ),
+            allUser[updateUser].imagen = req.file ? req.file.filename : infoUser.imagen;
 
-      fs.writeFileSync(usersFilePathFilePath, JSON.stringify(infoUser));  
-      res.redirect("/profile");
-    },
-    
-
+            User.updateList(allUser);
+            res.redirect("/user/profile");
+          }
+      }
+    }
   };
-  
-// Probando método de hasheo de contraseñas
-
-// console.log(userController.passwordHash("MabuttiDH24"));
-
-// console.log(userController.passwordHash("SsmithDH24"));
-
-// console.log(userController.passwordHash("MpereyraDH24"));
-
-// console.log(userController.passwordHash("MgomezDH24"));
-
-// console.log(userController.passwordHash("GdominguezDH24"));
-
-
-// Probando compare sync
-// console.log(bcrypt.compareSync("MabuttiDH24" , "$2a$10$Q/N.y/cCTGDASVe7jXlfXuQXL6R/bACboukDdSjHsHyv3./BSxfAy"));
-// console.log(bcrypt.compareSync("SsmithDH24" , "$2a$10$BWWIFCWKd7gxA57X1p2gK.efMlI2sfEqtDWYl6i5zbuOoIN2HY2Ei"));
-// console.log(bcrypt.compareSync("MpereyraDH24" , "$2a$10$f0kwB45GE8kVdI.W6gIvqOKE.wIxSM.XsIEpdRBIajG3AX/ZWVrvu"));
-// console.log(bcrypt.compareSync("MgomezDH24" , "$2a$10$jmeHLz.wcBHKov6WxHPbmORHs9tr5.Q8Dq9pfSCnoGYCY4qHZlyZC"));
-// console.log(bcrypt.compareSync("GdominguezDH24" , "$2a$10$UDKihaU/PuTghnDEboD1oOeIpOpJm3cSpu1YZqENJ.1l9bYQEDOve"));
 
   module.exports = userController;
