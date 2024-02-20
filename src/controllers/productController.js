dfdbea7a7a63620195125eb9078687a665f0a4d2
Marta <mabutti@gmail.com>
2024-02-20 18:12:31 -0300

    remove json product

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -14,13 +14,6 @@ const petSizeService = require('../model/petSizeService');
 
 const fs = require('fs');
 const path = require('path');
-const productsFilePath = path.join(__dirname, '../data/products.json');
-
-function getProducts() {
-  return JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-}
-
-const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const prodController = {
     indexProductController: (req, res) => {
