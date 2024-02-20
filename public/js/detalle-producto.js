506e8be7c419a0656970686770941fcf7cb33fd0
matigomez1611 <cpnmatiasgomez@gmail.com>
2024-02-20 13:22:52 -0300

    buttons edit and delete with validations

---
+++ public/js/detalle-producto.js
@@ -0,0 +1,14 @@
+// Capturamos el button de ELIMINAR
+let deleteButton = document.querySelector('button.delete');
+let aDeleteElement = document.querySelector('a.delete');
+
+// Agregamos mensaje de confirmación de borrado de producto
+
+deleteButton.addEventListener('click', (event) => {
+    if (confirm('¿Está seguro de eliminar este producto?')) {
+        alert('Producto eliminado');
+    } else {
+        event.preventDefault(); // Evita la redirección por defecto
+    }
+});
+
