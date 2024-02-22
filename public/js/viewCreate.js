b3231c51364c21bf0b3076d4caa294a733ccd70e
matigomez1611 <cpnmatiasgomez@gmail.com>
2024-02-22 20:26:18 -0300

    adding events on create form

--- public/js/viewCreate.js
+++ public/js/viewCreate.js
@@ -1,11 +1,50 @@
-function mostrarOpciones() {
-    var select1 = document.getElementById("categoria");
-    var opcionesDiv = document.getElementById("subCatSelect");
-    var opcionesDivPrentacion = document.getElementById("alimentoPresentacion");
-    var opcionesDivAccesorios = document.getElementById("subCatAccesorios");
-    var select2 = document.getElementById("subCat");
+window.addEventListener('load', () => {
+  // Capturamos los elementos para trabajar con el form y sus campos
+
+  // Capturamos el formulario
+  let form = document.querySelector('#form-alta-prod');
+
+  // Capturamos los div de subcategorias y presentacion
+  let divSubCatAlimentos = document.querySelector('#subCatSelect');
+  let divSubCatAccesorios = document.querySelector('#subCatAccesorios');
+  let divPresentacion = document.querySelector('#alimentoPresentacion');
+
+  // Agregamos eventos para validar categoria on time
+
+  form.categoria.addEventListener('change', () => {
+    if (parseInt(form.categoria.value) == 1) {
+      divSubCatAlimentos.style.display = 'block';
+      divPresentacion.style.display = 'block'
+      divSubCatAccesorios.style.display = 'none'
+    } else if (parseInt(form.categoria.value) == 2) {
+      divSubCatAccesorios.style.display = 'block'
+      divSubCatAlimentos.style.display = 'none';
+      divPresentacion.style.display = 'none'
+    } else {
+      console.log('no hay categoria aun');
+      divSubCatAlimentos.style.display = 'none';
+      divPresentacion.style.display = 'none';
+      divSubCatAccesorios.style.display = 'none'
+    }
+  });
+  
+  // Verificamos el value de categoria luego de cada submit con errores:
     
-    opcionesDiv.style.display = (select1.value == "1") ? "block" : "none";
-    opcionesDivPrentacion.style.display = (select1.value == "1") ? "block" : "none";
-    opcionesDivAccesorios.style.display = (select1.value == "2") ? "block" : "none";
+  if (parseInt(form.categoria.value) == 1) {
+    divSubCatAlimentos.style.display = 'block';
+    divPresentacion.style.display = 'block'
+    divSubCatAccesorios.style.display = 'none'
+  } else if (parseInt(form.categoria.value) == 2) {
+    divSubCatAccesorios.style.display = 'block'
+    divSubCatAlimentos.style.display = 'none';
+    divPresentacion.style.display = 'none'
+  } else {
+    console.log('no hay categoria aun');
+    divSubCatAlimentos.style.display = 'none';
+    divPresentacion.style.display = 'none';
+    divSubCatAccesorios.style.display = 'none'
   }
+
+});
+
+
