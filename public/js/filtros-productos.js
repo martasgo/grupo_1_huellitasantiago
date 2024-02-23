1675d6fb528c632deaacba4531297c5e58b87bf9
Marta <mabutti@gmail.com>
2024-02-23 12:05:22 -0300

    script to search function in mobile

---
+++ public/js/filtros-productos.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    const formulario = document.getElementById('searchProdMobile');
+    const select = document.getElementById('categoryList');
+
+    // Escuchar cambios en el select
+    select.addEventListener('change', () => {
+        // Obtener el valor seleccionado del select
+        const valorSelect = select.value;
+
+        // Completar el action del formulario con el valor seleccionado
+        formulario.action = valorSelect;
+    });
+}
