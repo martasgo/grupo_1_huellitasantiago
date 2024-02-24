cad662887a17ac745f59c86c1a8d058fed86b59e
SoleSmith <fernanda.smith.ss@gmail.com>
2024-02-24 13:34:08 -0300

    update userService

--- src/model/userService.js
+++ src/model/userService.js
@@ -1,17 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 const db = require("./database/models");
-//const Users = db.User;
 
 // Objeto User que contiene los métodos para manipular el JSON de usuarios
 const userService = {
-  // Ruta hacia el JSON de usuarios
-  //fileName: path.join(__dirname , "../data/users.json"),
-  // Conversión del JSON de usuarios a array para poder trabajarlo con JS
-
-  /* getData: function () {
-        return JSON.parse(fs.readFileSync(this.fileName , "utf-8"));
-    }, */
+  
   getData: async function () {
     try {
       return await db.User.findAll();
@@ -56,14 +49,6 @@ const userService = {
     }
   },
 
-  // Método para eliminar un usuario por su ID
-  /* delete: function (id) {
-        let allUsers = this.findAll();
-        let finalUsers = allUsers.filter (oneUser => oneUser.id !== id);
-        fs.writeFileSync(this.fileName , JSON.stringify(finalUsers , null , " ") );
-	    return true;
-    }, */
-
   delete: async (id) => {
     try {
       const result = await db.User.destroy({
@@ -77,20 +62,6 @@ const userService = {
       throw new Error("No se pudo procesar la solicitud correctamente");
     }
   },
-
-  /* deleteImagen: async (imagen) => {
-    try {
-      const result = await db.User.destroy({
-        where: {
-          imagen: imagen,
-        },
-      });
-      return result;
-    } catch (error) {
-      console.log(error);
-      throw new Error("No se pudo procesar la solicitud correctamente");
-    }
-  }, */
 
   deleteImagen:  async (img) => {
     try {    
@@ -125,24 +96,6 @@ const userService = {
     }
   },
 };
-/* updateList: function(user){
-        fs.writeFileSync(this.fileName, JSON.stringify(user), {
-            flag:"w",
-            encoding:"utf-8",
-        });
-	    return true;
-    }, */
 
-/*    updateList: function (req, res) {
-        userService.getByPk(req.params.id, req.body)
-          .then(() => res.redirect(`/userList/${req.params.id}/detalle`))
-          .catch((e) => res.send(error.message));
-    }, */
-
-/* deleteImagen: function(img){
-        let directorio = path.resolve(__dirname , "../../public/images/usuarios");
-        const rutaImagen = path.join(directorio, img);
-        fs.unlinkSync(rutaImagen);
-    } */
 
 module.exports = userService;
