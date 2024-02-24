e089ad439e5d8eeacd58014b4f3309111e4274c6
SoleSmith <fernanda.smith.ss@gmail.com>
2024-02-24 13:21:46 -0300

    changes userService

--- src/model/userService.js
+++ src/model/userService.js
@@ -1,50 +1,148 @@
 const fs = require("fs");
 const path = require("path");
+const db = require("./database/models");
+//const Users = db.User;
 
 // Objeto User que contiene los métodos para manipular el JSON de usuarios
-const User = {
-    // Ruta hacia el JSON de usuarios
-    fileName: path.join(__dirname , "../data/users.json"),
-    // Conversión del JSON de usuarios a array para poder trabajarlo con JS 
-    getData: function () {
+const userService = {
+  // Ruta hacia el JSON de usuarios
+  //fileName: path.join(__dirname , "../data/users.json"),
+  // Conversión del JSON de usuarios a array para poder trabajarlo con JS
+
+  /* getData: function () {
         return JSON.parse(fs.readFileSync(this.fileName , "utf-8"));
-    },
-    // Método que obtiene a todos los usuarios
-    findAll: function () {
-        return this.getData();
-    },
-    // Método para encontrar un usuario por ID
-    getByPk: function(id) {
-        let allUsers = this.findAll();
-        let userFound = allUsers.find(oneUser => oneUser.id === id);
-	    return userFound;
-    },
-    // Método para encontrar un usuario por campo de formulario
-    getByField: function(field , text) {
-        let allUsers = this.findAll();
-        let userFound = allUsers.find(oneUser => oneUser[field] === text);
-        return userFound;
-    },
-    
-    // Método para eliminar un usuario por su ID
-    delete: function (id) {
+    }, */
+  getData: async function () {
+    try {
+      return await db.User.findAll();
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  },
+
+  // Método que obtiene a todos los usuarios
+  findAll: async function () {
+    try {
+      return await this.getData();
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }, 
+
+  // Método para encontrar un usuario por ID
+  getByPk: async function (id) {
+    try {
+      const userFound = await db.User.findByPk(id);
+      return userFound;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  },
+  // Método para encontrar un usuario por campo de formulario
+  getByField: async function (text) {
+    try {
+      const userFound = await db.User.findOne({
+        where: {
+          email: text,
+        },
+      });
+      return userFound;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  },
+
+  // Método para eliminar un usuario por su ID
+  /* delete: function (id) {
         let allUsers = this.findAll();
         let finalUsers = allUsers.filter (oneUser => oneUser.id !== id);
         fs.writeFileSync(this.fileName , JSON.stringify(finalUsers , null , " ") );
 	    return true;
-    },
-    updateList: function(user){
+    }, */
+
+  delete: async (id) => {
+    try {
+      const result = await db.User.destroy({
+        where: {
+          id: id,
+        },
+      });
+      return result;
+    } catch (error) {
+      console.log(error);
+      throw new Error("No se pudo procesar la solicitud correctamente");
+    }
+  },
+
+  /* deleteImagen: async (imagen) => {
+    try {
+      const result = await db.User.destroy({
+        where: {
+          imagen: imagen,
+        },
+      });
+      return result;
+    } catch (error) {
+      console.log(error);
+      throw new Error("No se pudo procesar la solicitud correctamente");
+    }
+  }, */
+
+  deleteImagen:  async (img) => {
+    try {    
+    let directorio = path.resolve(__dirname, "../../public/images/usuarios");
+    const rutaImagen = path.join(directorio, img);
+    fs.unlinkSync(rutaImagen);
+  }catch (error){
+    console.log(error);
+      throw new Error("No se pudo procesar la solicitud correctamente");
+    }
+  },
+
+  updateList: async (user, id) => {
+    try {
+      return await db.User.update(user, {
+        where: {
+          id: id,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      throw new Error("No se pudo procesar la solicitud correctamente");
+    }
+  },
+
+  addUser: async function (user) {
+    try {
+      return await db.User.create(user);
+    } catch (error) {
+      console.log(error);
+      throw new Error("No se pudo procesar la solicitud correctamente");
+    }
+  },
+};
+/* updateList: function(user){
         fs.writeFileSync(this.fileName, JSON.stringify(user), {
             flag:"w",
             encoding:"utf-8",
         });
 	    return true;
-    },
-    deleteImagen: function(img){
+    }, */
+
+/*    updateList: function (req, res) {
+        userService.getByPk(req.params.id, req.body)
+          .then(() => res.redirect(`/userList/${req.params.id}/detalle`))
+          .catch((e) => res.send(error.message));
+    }, */
+
+/* deleteImagen: function(img){
         let directorio = path.resolve(__dirname , "../../public/images/usuarios");
         const rutaImagen = path.join(directorio, img);
         fs.unlinkSync(rutaImagen);
-    }
-};
+    } */
 
-module.exports = User;
+module.exports = userService;
