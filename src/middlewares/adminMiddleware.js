ef0aec385f935404d7359cb4283b72a94c9c7afa
SoleSmith <fernanda.smith.ss@gmail.com>
2024-02-26 13:06:20 -0300

    change logil operator - conditional if adminMiddleware

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,7 +1,7 @@
 const userService = require ("../model/userService");
 
 function adminMiddleware (req,res,next) {
-    if (req.session.userLogged.id_categoria !== 1) {
+    if (req.session.userLogged.id_categoria != 1) {
         let adminError = {};
         return res.render('../views/users/profile.ejs', {
             title: 'Perfil de usuario',
