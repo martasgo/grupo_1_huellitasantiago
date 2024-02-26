357c48f710bd0880710b3b8cb223b09cabbb9fa4
matigomez1611 <cpnmatiasgomez@gmail.com>
2024-02-26 19:40:47 -0300

    Brand model fix

--- src/model/database/models/Brand.js
+++ src/model/database/models/Brand.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
     let alias = 'Brand';
     let cols = {    
         id: {
-            type: dataTypes.INTEGER,
+            type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true,
             allowNull: false
