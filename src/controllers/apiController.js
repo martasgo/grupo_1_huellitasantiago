c1b6b6cff76a3b0a57c486433a258c26b98bd5e6
matigomez1611 <cpnmatiasgomez@gmail.com>
2024-03-20 14:52:10 -0300

    methods for api products

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -6,13 +6,39 @@ const cartProductService = require ('../model/cartProductService');
 const apiController = {
 
     allProducts: async (req, res) => {
-        let products = await productService.getAll();
-        return res.json(products)
+        let alimentosProducts = await productService.getByCategory(1);
+        let accesoriosProducts = await productService.getByCategory(2);
+        let cuidadoHigieneProducts = await productService.getByCategory(3);
+        let ropaProducts = await productService.getByCategory(4);
+        let allProducts = await productService.getAll();
+        let results = {
+            count: allProducts.length,
+            countByCategory: {
+                Alimentos: alimentosProducts.length,
+                Accesorios: accesoriosProducts.length,
+                Cuidado: cuidadoHigieneProducts.length,
+                Ropa: ropaProducts.length
+            },
+            products: allProducts
+        }
+        return res.json(results)
     },
 
     productById: async (req, res) => {
         let product = await productService.getByPk(req.params.id);
-        return res.json(product)
+        // Convertir las relaciones a un array en el objeto principal
+        let productJSON = product.toJSON(); // Convierte el objeto Sequelize a JSON
+
+        // Iterar sobre las relaciones y convertirlas a arrays si son objetos
+        for (let key in productJSON) {
+            if (Array.isArray(productJSON[key])) { // Si la propiedad ya es un array, continuar
+                continue;
+            } else if (typeof productJSON[key] === 'object' && productJSON[key] !== null) { // Si la propiedad es un objeto, convertir a array
+                productJSON[key] = [productJSON[key]]; // Convertir el objeto a un array de un solo elemento
+            }
+        }
+
+        return res.json(productJSON);
     },
 
     checkout: async (req, res) => {
