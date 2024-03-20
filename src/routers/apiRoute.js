eefe19ea87757f4c3f7e1a8cda9b8d03eb0a4e2b
matigomez1611 <cpnmatiasgomez@gmail.com>
2024-03-20 14:44:00 -0300

    users api routes

--- src/routers/apiRoute.js
+++ src/routers/apiRoute.js
@@ -5,13 +5,17 @@ const authMiddleware = require ("../middlewares/authMiddleware");
 const router = Router();
 
 const routes = {
-  allProducts: '/products',
-  productById: '/product/:id',
-  checkout: '/checkout'
+  // allProducts: '/products',
+  // productById: '/product/:id',
+  checkout: '/checkout',
+  allUsers: '/users',
+  userById: '/users/:id'
 };
 
-router.get(routes.productById, controller.productById);
-router.get(routes.allProducts, controller.allProducts);
-router.post(routes.checkout, authMiddleware, controller.checkout)
+// router.get(routes.productById, controller.productById);
+// router.get(routes.allProducts, controller.allProducts);
+router.post(routes.checkout, authMiddleware, controller.checkout);
+router.get(routes.allUsers, controller.allUsers);
+router.get(routes.userById, controller.userById);
 
 module.exports = router;
