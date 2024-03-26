f5a173ffedc3d205c3fa33060f94bda9e47a628d
SoleSmith <fernanda.smith.ss@gmail.com>
2024-03-26 17:02:15 -0300

    add soft delete and refactor login service

--- src/model/userService.js
+++ src/model/userService.js
@@ -1,10 +1,13 @@
+const db = require("./database/models");
+const Op = db.Sequelize.Op;
 const fs = require("fs");
 const path = require("path");
-const db = require("./database/models");
+
+const { validationResult } = require("express-validator");
+let bcrypt = require("bcryptjs");
 
 // Objeto User que contiene los métodos para manipular el JSON de usuarios
 const userService = {
-  
   getData: async function () {
     try {
       return await db.User.findAll();
@@ -22,7 +25,7 @@ const userService = {
       console.log(error);
       return [];
     }
-  }, 
+  },
 
   // Método para encontrar un usuario por ID
   getByPk: async function (id) {
@@ -35,41 +38,39 @@ const userService = {
     }
   },
   // Método para encontrar un usuario por campo de formulario
-  getByField: async function (text) {
+  getByField: async function (email) {
     try {
-      const userFound = await db.User.findOne({
+      return await db.User.findOne({
         where: {
-          email: text,
+          email: email,
         },
       });
-      return userFound;
+      
     } catch (error) {
-      console.log(error);
-      return [];
+      return{errors: "Error al buscar el usuario"};
     }
   },
 
   delete: async (id) => {
     try {
-      const result = await db.User.destroy({
-        where: {
-          id: id,
-        },
-      });
-      return result;
+      const userToUpdate = await db.User.findByPk(id);
+      if (!userToUpdate) {
+        throw new Error("Usuario no encontrado");
+      }
+      userToUpdate.activo = false; 
+      await userToUpdate.save(); 
+      return userToUpdate;
     } catch (error) {
-      console.log(error);
       throw new Error("No se pudo procesar la solicitud correctamente");
     }
   },
 
-  deleteImagen:  async (img) => {
-    try {    
-    let directorio = path.resolve(__dirname, "../../public/images/usuarios");
-    const rutaImagen = path.join(directorio, img);
-    fs.unlinkSync(rutaImagen);
-  }catch (error){
-    console.log(error);
+  deleteImagen: async (img) => {
+    try {
+      let directorio = path.resolve(__dirname, "../../public/images/usuarios");
+      const rutaImagen = path.join(directorio, img);
+      fs.unlinkSync(rutaImagen);
+    } catch (error) {
       throw new Error("No se pudo procesar la solicitud correctamente");
     }
   },
@@ -82,20 +83,51 @@ const userService = {
         },
       });
     } catch (error) {
-      console.log(error);
       throw new Error("No se pudo procesar la solicitud correctamente");
     }
   },
 
-  addUser: async function (user) {
+  addUser: async function (newUser, userInDB) {
     try {
-      return await db.User.create(user);
+      if(userInDB && userInDB.activo == false){
+        return await db.User.update(newUser, {
+          where: {
+            id: userInDB.id,
+          },
+        });
+      }
+      return await db.User.create(newUser);
     } catch (error) {
-      console.log(error);
       throw new Error("No se pudo procesar la solicitud correctamente");
     }
   },
-};
 
+  login: async function (userData) {
+    try {
+    const errors = validationResult(userData);
+    if (!errors.isEmpty()) {
+      const mappedErrors = errors.mapped();
+      return { errors: mappedErrors };
+    }    
+    const userToLogin = await userService.getByField(userData.email);
+    
+    if (!userToLogin || userToLogin.activo == false) {
+      console.log(errors)
+      return { errors: { email: { msg: "Este email no se encuentra registrado" } } };
+    
+    }
+    const isOkThePassword = bcrypt.compareSync(
+      userData.contraseña,
+      userToLogin.contrasenia
+    );
+    if (!isOkThePassword) {
+      return { errors: { contrasenia: { msg: "Credenciales inválidas" } } };
+    }        
+    return {user: userToLogin};  
+  } catch (error) {
+    return { errors: { general: { msg: "Error en el servidor al iniciar sesión" } } };
+  }
+}
+}
 
 module.exports = userService;
